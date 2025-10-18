f21cf76c5a75139369f24b4cbd4dc900d41b8678
DaniilBandarenka <dbondarenko.post@gmail.com>
2025-10-18 14:03:48 +0200

    break CI

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-
 from app.routers.endpoints import router
+
 
 app = FastAPI()
 
 app.include_router(router)
-
-
-@app.get("/ping", tags=["Ping"], summary="Pong :D")
+@app.get("/ping", tags= ["Ping"], summary="Pong :D")
 async def ping():
+
+
     return "pong"
